16bee47c887e640d8989a4434d7c9decf8216b88
proffee <bogdan.boiko.ws@gmail.com>
2017-06-30 07:04:33 +0300

    Поправка...
    Немного подправил сервлет SelectTAble

--- SelectTable.java
+++ SelectTable.java
@@ -6,10 +6,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import javax.swing.table.TableModel;
 
 import controllere.JpaController;
+
 
 /**
  * Servlet implementation class SelectTable
@@ -27,27 +27,25 @@ public class SelectTable extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-    
     public static JpaController getController() {
- 		if(controller==null)
- 			controller = new JpaController();
- 		return controller;
- 	}
- 	
- 	public static TableModel getTableModel() {
- 		return getController().getModel(className);
- 	}
+		if(controller==null)
+			controller = new JpaController();
+		return controller;
+	}
+	
+	public static TableModel getTableModel() {
+		return getController().getModel(className);
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {String tableName = request.getParameter("tableName");
-	HttpSession session = request.getSession();
-	JpaController controller = (JpaController) session.getAttribute("controller");
-	TableModel tableModel = controller.getModel(tableName);
-	session.setAttribute("tableName", tableName);
-	session.setAttribute("tableModel", tableModel);
-	request.getRequestDispatcher("showTable.jsp").forward(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		className = request.getParameter("className");
+		request.setAttribute("model", getTableModel());
+		request.setAttribute("className", className);
+		request.getRequestDispatcher("showTable.jsp").forward(request, response);
 
 	}
 
